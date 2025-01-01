11d4acd2c168ee2a8db2b86fc6f50226af0458b8
Breno Brollo <brenobrolloo@gmail.com>
2025-01-01 09:07:12 -0300

    js e alguns botoes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.sr = ScrollReveal({reset:true});
 
-sr.reveal('.grid-conteudo', {duration: 4000});
-sr.reveal('.video', {duration: 5000});
+sr.reveal('.grid-conteudo', {duration: 3000});
+sr.reveal('.video', {duration: 8000});
+sr.reveal('.grid-equipe', {rotate: {x: 0, y: 100, z:0}, duration: 6000});
+
